60a8b380737fbab5d365fcb68dc073daea243990
joshfarhi <joshfarhi@icloud.com>
2019-02-01 17:21:51 -0500

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,9 +14,6 @@ connection.connect(function (err) {
   readProducts();
 });
 
-readProducts();
-userInput();
-
 function readProducts() {
   console.log("...\n");
   connection.query("SELECT * FROM products", function (err, res) {
@@ -32,32 +29,48 @@ function readProducts() {
       console.log("Quantity: " + res[i].stock_quantity);
       console.log("///////////////////////////////////////")
     }
+    userInput(res);
   });
 }
 
-
-
-function userInput() {
+function userInput(results) {
   var questions = [
     {
       type: 'input',
       name: 'userI',
-      message: 'Enter the product ID '
+      message: 'Enter the product ID: '
     },
     {
       type: 'input',
       name: 'userQ',
-      message: 'Enter the quantity '
+      message: 'Enter the quantity: '
     }]; inquirer.prompt(questions).then(function (answers) {
       var pID = answers.userI;
       var uQ = answers.userQ;
-
-      
-
+      var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_id == pID) {
+            chosenItem = results[i];
+            
+          }
+        }
+      if (chosenItem.stock_quantity >= uQ) {
+        connection.query("UPDATE products SET ? WHERE ?",
+        [{
+          stock_quantity: chosenItem.stock_quantity - uQ
+        }, 
+        {item_id: chosenItem.item_id}
+      ])
+      console.log("Order successfully placed!")
+      console.log("Total Price: " + uQ * chosenItem.price)
+      } else {
+        console.log("Please enter a lower quantity")
+      }
+        
 
     });
 
-
+connection.end();
 }
 
 
