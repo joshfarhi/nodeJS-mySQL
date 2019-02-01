86ff32269eaa3cc79ca283346785b45296273087
joshfarhi <joshfarhi@icloud.com>
2019-02-01 11:17:23 -0500

    uncomplete but attempted

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,65 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "bamazon"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM products", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result[i]);
+const connection = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "bamazon"
   });
+
+connection.connect(function (err) {
+  if (err) throw err;
+  readProducts();
 });
+
+readProducts();
+userInput();
+
+function readProducts() {
+  console.log("...\n");
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    // console.log(res);
+
+    for (i = 0; i < res.length; i++) {
+      console.log("Item: " + res[i].item_id);
+      console.log("Product: " + res[i].product_name);
+      console.log("Department: " + res[i].department_name);
+      console.log("Price: " + res[i].price);
+      console.log("Quantity: " + res[i].stock_quantity);
+      console.log("///////////////////////////////////////")
+    }
+  });
+}
+
+
+
+function userInput() {
+  var questions = [
+    {
+      type: 'input',
+      name: 'userI',
+      message: 'Enter the product ID '
+    },
+    {
+      type: 'input',
+      name: 'userQ',
+      message: 'Enter the quantity '
+    }]; inquirer.prompt(questions).then(function (answers) {
+      var pID = answers.userI;
+      var uQ = answers.userQ;
+
+      
+
+
+    });
+
+
+}
+
+
+
+
